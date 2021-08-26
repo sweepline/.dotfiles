fd3a59f901674964e5ab5fb74b865363f9ad1d43
Mikkel Holmer Pedersen <mikkel@scalgo.com>
2021-08-26 13:12:34 +0200

    Move telescope config to lua

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -6,6 +6,12 @@ utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear Highlights
 utils.map('i', 'jk', '<Esc>') -- jk to escape
 utils.map('i', '<C-Space>', '<C-x><C-o>')
 
+
+utils.map('n', '<Leader>ff', '<cmd>Telescope find_files<CR>')
+utils.map('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>')
+utils.map('n', '<Leader>fb', '<cmd>Telescope buffers<CR>')
+utils.map('n', '<Leader>fh', '<cmd>Telescope help_tags<CR>')
+
 utils.map('n', '<Leader>gs', '<cmd>Git<CR>') -- Git status
 
 utils.map('n', '<C-n>', '<cmd>NvimTreeToggle<CR>')
