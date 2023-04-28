d0a206ab6a547ed3e79f6ec7d9379bb157e04265
Mikkel Holmer Pedersen <mikkel@scalgo.com>
2023-04-28 14:45:44 +0200

    Add screen recording

---
+++ sway/.config/sway/screen-record.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+fuser -sk -SIGINT "$0"
+test "$1" = off && exit 0
+[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -enx "$0" "$0" "$@" || :
+exec wf-recorder -f ~/Videos/recording_$(date +%F_%H-%M-%S).mp4 -g "$(slurp)"
