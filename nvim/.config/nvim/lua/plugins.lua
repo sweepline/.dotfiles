cc3a88f27f60ad09bca318885d1b25267fed22b0
Mikkel Holmer Pedersen <m@esmi.dk>
2026-02-06 14:46:45 +0100

    Update to new treesitter from main branch

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -17,6 +17,7 @@ return require("lazy").setup({
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
 	},
 	{
--- nvim/.config/nvim/lua/treesitter.lua
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -1,21 +1,28 @@
-local configs = require("nvim-treesitter.configs")
+local treesitter = require("nvim-treesitter")
 
-configs.setup {
-	highlight = { enable = true },
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "<M-w>",
-			node_incremental = "<M-w>",
-			scope_incremental = "<M-e>",
-			node_decremental = "<M-C-w>"
-		}
-	},
-	indent = { enable = true }
-}
+-- Enable Treesitter highligh to all filetypes that there are parsers for.
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    if
+        -- is arg2 in arg1
+        vim.list_contains(
+          -- all installed parsers
+          treesitter.get_installed(),
+          -- get parser for current language
+          vim.treesitter.language.get_lang(args.match)
+        )
+    then
+      vim.treesitter.start(args.buf)
+    end
+  end,
+})
 
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+-- Treesitter folding
+vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo[0][0].foldmethod = 'expr'
 vim.o.foldlevelstart = 20 -- start folded if more than 20
-vim.wo.foldnestmax = 3
-vim.wo.foldminlines = 5
+vim.wo[0][0].foldnestmax = 3
+vim.wo[0][0].foldminlines = 5
+
+-- Treesitter indentation
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
