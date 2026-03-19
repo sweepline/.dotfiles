ad72e939f529daab9658aa76833a1a2071314a4c
Mikkel Holmer Pedersen <mikkel@scalgo.com>
2026-03-19 12:27:16 +0100

    Correctly configure treesitter textobjects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -36,15 +36,92 @@ return {
 			-- Disable entire built-in ftplugin mappings to avoid conflicts.
 			-- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
 			vim.g.no_plugin_maps = true
-
-			-- Or, disable per filetype (add as you like)
-			-- vim.g.no_python_maps = true
-			-- vim.g.no_ruby_maps = true
-			-- vim.g.no_rust_maps = true
-			-- vim.g.no_go_maps = true
 		end,
 		config = function()
 			-- put your config here
+			require("nvim-treesitter-textobjects").setup {
+				select = {
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+					-- You can choose the select mode (default is charwise 'v')
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * method: eg 'v' or 'o'
+					-- and should return the mode ('v', 'V', or '<c-v>') or a table
+					-- mapping query_strings to modes.
+					selection_modes = {
+						['@parameter.outer'] = 'v', -- charwise
+						['@function.outer'] = 'V', -- linewise
+						-- ['@class.outer'] = '<c-v>', -- blockwise
+					},
+					-- If you set this to `true` (default is `false`) then any textobject is
+					-- extended to include preceding or succeeding whitespace. Succeeding
+					-- whitespace has priority in order to act similarly to eg the built-in
+					-- `ap`.
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * selection_mode: eg 'v'
+					-- and should return true of false
+					include_surrounding_whitespace = false,
+				},
+			}
 		end,
+		keys = {
+			-- Select around function (includes signature)
+			{
+				"am",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+				end,
+				mode = { "x", "o" },
+				desc = "around function"
+			},
+			-- Select inside function (body only)
+			{
+				"im",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@function.inner",
+						"textobjects")
+				end,
+				mode = { "x", "o" },
+				desc = "inner function"
+			},
+			-- Select around class
+			{
+				"ac",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@class.outer",
+						"textobjects")
+				end,
+				mode = { "x", "o" },
+				desc = "around class"
+			},
+			-- Select inside class (body only)
+			{
+				"ic",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@class.inner",
+						"textobjects")
+				end,
+				mode = { "x", "o" },
+				desc = "inner class"
+			},
+			-- Select current scope (block, function, etc.)
+			{
+				"as",
+				function()
+					require("nvim-treesitter-textobjects.select").select_textobject("@local.scope",
+						"locals")
+				end,
+				mode = { "x", "o" },
+				desc = "around scope"
+			},
+			-- Swap current parameter with the next one
+			{ "<leader>tn", function() require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner") end,     desc = "swap with next object" },
+			-- Swap current parameter with the previous one
+			{ "<leader>tp", function() require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.outer") end, desc = "swap with previous object" },
+		}
 	}
 }
